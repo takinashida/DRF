698c579bfa298ab0a30dca0e345927079c804ee4
takinashida <takinashida@gmail.com>
2025-11-23 17:42:10 +0300

    TESTS IS DONE

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
-from materials.models import Lesson, Course
-
+from materials.models import Lesson, Course, Subscription
+from materials.validators import validate_url
 
 
 class  CourseSerializer(serializers.ModelSerializer):
@@ -24,9 +24,22 @@ class  CourseSerializer(serializers.ModelSerializer):
 
 
 class  LessonSerializer(serializers.ModelSerializer):
+    url = serializers.CharField(validators=[validate_url])
 
     class Meta:
         model= Lesson
         fields = "__all__"
+
+
+class  SubscriptionSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model= Subscription
+        fields = "__all__"
+
+    def get_is_subscribed(self, obj):
+        user = self.context["request"].user
+        return obj.subscriptions.filter(user=user).exists()
 
 
